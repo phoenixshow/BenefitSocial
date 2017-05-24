ccafd50288eb427d0af184a536f5081ef5c6d122
phoenixshow <965550123@qq.com>
2017-05-24 17:08:57 +0800

    联系人列表的实现

--- app/src/main/java/com/phoenix/social/controller/fragment/ContactFragment.java
+++ app/src/main/java/com/phoenix/social/controller/fragment/ContactFragment.java
@@ -1,10 +1,24 @@
 package com.phoenix.social.controller.fragment;
 
-import android.support.v4.app.Fragment;
+import android.view.View;
+
+import com.hyphenate.easeui.ui.EaseContactListFragment;
+import com.phoenix.social.R;
 
 /**
  * 联系人列表
  */
 
-public class ContactFragment extends Fragment {
+public class ContactFragment extends EaseContactListFragment {
+    @Override
+    protected void initView() {
+        super.initView();
+
+        //头布局显示右侧+号
+        titleBar.setRightImageResource(R.drawable.em_add);
+
+        //添加头布局
+        View headerView = View.inflate(getActivity(), R.layout.header_fragment_contact, null);
+        listView.addHeaderView(headerView);
+    }
 }
