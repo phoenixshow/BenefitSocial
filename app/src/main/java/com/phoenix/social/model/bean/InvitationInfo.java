a05ad0b0179f8c155edc15e19c491fd910746b98
phoenixshow <965550123@qq.com>
2017-05-26 03:33:07 +0800

    创建邀请信息表及该表的操作类

--- app/src/main/java/com/phoenix/social/model/bean/InvitationInfo.java
+++ app/src/main/java/com/phoenix/social/model/bean/InvitationInfo.java
@@ -20,6 +20,38 @@ public class InvitationInfo {
         this.status = status;
     }
 
+    public UserInfo getUser() {
+        return user;
+    }
+
+    public void setUser(UserInfo user) {
+        this.user = user;
+    }
+
+    public GroupInfo getGroup() {
+        return group;
+    }
+
+    public void setGroup(GroupInfo group) {
+        this.group = group;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public InvitationStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(InvitationStatus status) {
+        this.status = status;
+    }
+
     public enum InvitationStatus{
         // 联系人邀请信息状态
         NEW_INVITE,// 新邀请
@@ -27,36 +59,24 @@ public class InvitationInfo {
         INVITE_ACCEPT_BY_PEER,// 邀请被接受
 
         // --以下是群组邀请信息状态--
+        NEW_GROUP_INVITE,//收到邀请去加入群
+        NEW_GROUP_APPLICATION,//收到申请群加入
+        GROUP_INVITE_ACCEPTED,//群邀请已经被对方接受
+        GROUP_APPLICATION_ACCEPTED,//群申请已经被批准
+        GROUP_ACCEPT_INVITE,//接受了群邀请
+        GROUP_ACCEPT_APPLICATION,//批准的群加入申请
+        GROUP_REJECT_INVITE,//拒绝了群邀请
+        GROUP_REJECT_APPLICATION,//拒绝了群申请加入
+        GROUP_INVITE_DECLINED,//群邀请被对方拒绝
+        GROUP_APPLICATION_DECLINED;//群申请被拒绝
 
-        //收到邀请去加入群
-        NEW_GROUP_INVITE,
-
-        //收到申请群加入
-        NEW_GROUP_APPLICATION,
-
-        //群邀请已经被对方接受
-        GROUP_INVITE_ACCEPTED,
-
-        //群申请已经被批准
-        GROUP_APPLICATION_ACCEPTED,
-
-        //接受了群邀请
-        GROUP_ACCEPT_INVITE,
-
-        //批准的群加入申请
-        GROUP_ACCEPT_APPLICATION,
-
-        //拒绝了群邀请
-        GROUP_REJECT_INVITE,
-
-        //拒绝了群申请加入
-        GROUP_REJECT_APPLICATION,
-
-        //群邀请被对方拒绝
-        GROUP_INVITE_DECLINED,
-
-        //群申请被拒绝
-        GROUP_APPLICATION_DECLINED
+        public static InvitationStatus valueOf(int ordinal) {
+            if (ordinal < 0 || ordinal >= values().length) {
+//                throw new IndexOutOfBoundsException("Invalid ordinal");
+                return null;
+            }
+            return values()[ordinal];
+        }
     }
 
     @Override
--- app/src/main/java/com/phoenix/social/model/db/DBHelper.java
+++ app/src/main/java/com/phoenix/social/model/db/DBHelper.java
@@ -5,13 +5,11 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import com.phoenix.social.model.dao.ContactTable;
-
-import static android.R.attr.version;
+import com.phoenix.social.model.dao.InviteTable;
 
 /**
  * Created by flashing on 2017/5/24.
  */
-
 public class DBHelper extends SQLiteOpenHelper {
     public DBHelper(Context context, String name) {
         super(context, name, null, 1);
@@ -23,6 +21,7 @@ public class DBHelper extends SQLiteOpenHelper {
         db.execSQL(ContactTable.CREATE_TAB);
 
         //创建邀请信息的表
+        db.execSQL(InviteTable.CREATE_TAB);
     }
 
     @Override
