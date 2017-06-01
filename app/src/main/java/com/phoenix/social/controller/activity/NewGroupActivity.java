a687d473a3f3fbc6211ed0391656f06815e2c816
phoenixshow <965550123@qq.com>
2017-06-01 21:40:33 +0800

    实现群列表页面，点击条目进入群会话详情页面，实现创建新群的点击监听，并在页面可见时刷新页面

---
+++ app/src/main/java/com/phoenix/social/controller/activity/NewGroupActivity.java
@@ -0,0 +1,16 @@
+package com.phoenix.social.controller.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.phoenix.social.R;
+
+//创建新群
+public class NewGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_group);
+    }
+}
--- app/src/main/java/com/phoenix/social/controller/fragment/ContactFragment.java
+++ app/src/main/java/com/phoenix/social/controller/fragment/ContactFragment.java
@@ -21,6 +21,7 @@ import com.hyphenate.exceptions.HyphenateException;
 import com.phoenix.social.R;
 import com.phoenix.social.controller.activity.AddContactActivity;
 import com.phoenix.social.controller.activity.ChatActivity;
+import com.phoenix.social.controller.activity.GroupListActivity;
 import com.phoenix.social.controller.activity.InviteActivity;
 import com.phoenix.social.model.Model;
 import com.phoenix.social.model.bean.UserInfo;
@@ -79,9 +80,22 @@ public class ContactFragment extends EaseContactListFragment {
         setContactListItemClickListener(new EaseContactListItemClickListener() {
             @Override
             public void onListItemClicked(EaseUser user) {
+                if (user == null){
+                    return;
+                }
                 Intent intent = new Intent(getActivity(), ChatActivity.class);
                 //传递参数
                 intent.putExtra(EaseConstant.EXTRA_USER_ID, user.getUsername());
+                startActivity(intent);
+            }
+        });
+
+        //跳转到群组列表页面
+        LinearLayout ll_contact_group = (LinearLayout) headerView.findViewById(R.id.ll_contact_group);
+        ll_contact_group.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), GroupListActivity.class);
                 startActivity(intent);
             }
         });
